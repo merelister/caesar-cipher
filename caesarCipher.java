7f73c3b65850665b5cf37273134b8f5e90132503
meredith <listermeredith@gmail.com>
2020-09-15 16:15:42 -0700

    method to encrypt text

--- caesarCipher.java
+++ caesarCipher.java
@@ -1,4 +1,5 @@
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 class caesarCipher {
@@ -6,19 +7,38 @@ class caesarCipher {
 	public static void main(String[] args) {
 		System.out.println("What is your message?");
 		Scanner in = new Scanner(System.in);
-		String message = in.nextLine();
+		String message = in.nextLine().toUpperCase();
+
 		System.out.println("Would you like to encrypt [1] or decrypt [2] your message?");
-	int choice = in.nextInt();
+	try {
+		int choice = in.nextInt();
+	
 		if (choice==1) 
 				encryptText(message);
 		else if (choice==2)
 				decryptText(message);
-		else 
-			System.out.println("Invalid option.");
+	} catch (InputMismatchException E) {
+		System.out.println("Invalid option."); //program ends
+	}
+			
 		
 	}
 	
-	public static void encryptText(String s) {}	
+	public static void encryptText(String s) {
+		String out = "";
+		for (int i=0; i < s.length(); i++) {
+			int c = (int)s.charAt(i);
+			if (c>=68 && c<=93) { //if c is a letter
+			c += 3; //caesar cipher shift each letter forward by 3
+			if (c>93) c = c-68; //loop around to beginning of alphabet
+			}
+		
+			out = out + (char)c;
+			
+		}
+		System.out.println(out);
+		
+	}	
 	
 	public static void decryptText(String s) {}
 }
